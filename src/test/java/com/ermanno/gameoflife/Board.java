8eaf8a25cfc480470a8be240326542e9661fe679
ermanno <moser.ermanno@gmail.com>
2017-01-31 20:55:28 +0100

    we don't need a cell concept, at least for the moment

--- src/test/java/com/ermanno/gameoflife/Board.java
+++ src/test/java/com/ermanno/gameoflife/Board.java
@@ -3,9 +3,9 @@ package com.ermanno.gameoflife;
 public class Board {
     private int width;
     private int height;
-    private Cell[][] board = new Cell[width][height];
+    private boolean[][] board = new boolean[width][height];
     
-    public Board(Cell[][] board) {
+    public Board(boolean[][] board) {
         int width = board.length;
         if (width > 0) {
             height = board[0].length;
@@ -13,18 +13,14 @@ public class Board {
         this.board = board;
     }
     
-    public Cell cells(int x, int y) {
-        return board[x][y];
-    }
-    
     public void evolve() {
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                int numAliveNeighbours = countAliveNeighbours(board[x][y]);
+                int numAliveNeighbours = countAliveNeighbours(x, y);
                 if (numAliveNeighbours <= 1 || numAliveNeighbours >= 4) {
-                    board[x][y].setAlive(false);
+                    board[x][y] = false;
                 } else {
-                    board[x][y].setAlive(true);
+                    board[x][y] = true;
                 }
             }
         }
@@ -34,14 +30,14 @@ public class Board {
         return (posX >= 0 && posX < width && posY >= 0 && posY < height);
     }
     
-    public int countAliveNeighbours(Cell cell) {
+    public int countAliveNeighbours(int cellX, int cellY) {
         int numAlive = 0;
         for (int x = -1; x <= 1; x += 2) {
             for (int y = -1; y <= 1; y += 2) {
-                int posX = cell.getX() + x;
-                int posY = cell.getY() + y;
+                int posX = cellX + x;
+                int posY = cellY + y;
                 if (isInsideBoard(posX, posY)) {
-                    if (board[posX][posY].isAlive())
+                    if (board[posX][posY])
                         numAlive++;
                 }
             }
--- src/test/java/com/ermanno/gameoflife/EngineTest.java
+++ src/test/java/com/ermanno/gameoflife/EngineTest.java
@@ -8,15 +8,14 @@ public class EngineTest {
 
     @Test
     public void testSolitude() {
-        Cell[][] cells = { 
-                { new Cell(0, 0, true), new Cell(0, 1, false), new Cell(0, 2, false) },
-                { new Cell(1, 0, false), new Cell(1, 1, true), new Cell(1, 2, false) },
-                { new Cell(2, 0, false), new Cell(2, 1, false), new Cell(2, 2, false) } 
+        boolean[][] cells = { 
+                { true, false, false },
+                { false, true, false },
+                { false, false, false } 
                 };
         Board board = new Board(cells);
-        assertEquals(1, board.countAliveNeighbours(board.cells(1, 1)));
+        assertEquals(1, board.countAliveNeighbours(1, 1));
         board.evolve();
-        
     }
 
 }
